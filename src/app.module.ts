cf8e668910ac116db4af64bc1c23f03f5525f6e4
Sofia Rovaris <sofiarovaris@gmail.com>
2023-11-16 21:39:11 -0300

    feat: add database config, enviroment and refactor readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      // entities: EntitiesList,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
